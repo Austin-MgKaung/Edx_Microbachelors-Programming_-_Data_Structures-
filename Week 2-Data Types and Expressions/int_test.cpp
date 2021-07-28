16d5c191ef997327f2c95ca5e9e50dbefe301c9e
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-07-28 19:42:34 +0630

    integer Example program (int_test.cpp)

---
+++ Week 2-Data Types and Expressions/int_test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    x = 6 ;
+    y = - 6 ;
+    cout<<x+2<<"\n";
+    y = x +2;
+    x+2;
+    cout<<"x "<<x<<endl;
+    return 0;
+}
