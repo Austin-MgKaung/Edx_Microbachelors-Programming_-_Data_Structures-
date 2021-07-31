6f0c3dabb8205d5df53f5897523a74e4e02dc5e2
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-07-31 23:32:02 +0630

    Classify character Program

---
+++ Week 4-Branching Statements/Classifychar.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    char userCh;
+
+    cout<<"Please enter a character :"<<endl;
+    cin>>userCh;
+
+    if (userCh >= 'a' && userCh <= 'z')
+    {
+        cout<<userCh<<" is a lower case letter."<<endl;
+    }
+    else if (userCh >= 'A' && userCh <= 'Z')
+    {
+        cout<<userCh<<" is a Upper case letter."<<endl;
+    }
+    else if (userCh >= '0' && userCh <= '9')
+    {
+        cout<<userCh<<" is a Digit."<<endl;
+    }
+    else
+    {
+        cout<<userCh<<" is ont alpha-numeric character."<<endl;
+    }
+    return 0;
+}
