0c34fd565bac78f48e983ae5f95bbf41cd78ab90
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-03 22:48:41 +0630

    Leap Year Function Program

---
+++ Week 10-Functions Labs/isleapyearf.cpp
@@ -0,0 +1,16 @@
+bool isleapyear(int inyear)
+{
+    if (inyear % 4== 0 && inyear % 100 != 0)
+    {   
+        return true;
+    }
+    else 
+    {   
+        if ( inyear % 100 == 0 && inyear % 400 == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
