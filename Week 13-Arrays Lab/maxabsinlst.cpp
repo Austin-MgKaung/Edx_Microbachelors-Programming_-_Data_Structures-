679e1ade72511f6ad27d81b47d139d6fd0c5eed5
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-04 00:31:07 +0630

    Max abs num in Array Lab Program

---
+++ Week 13-Arrays Lab/maxabsinlst.cpp
@@ -0,0 +1,22 @@
+int maxabsinlist(int lst[],int size)
+{
+    int max_num = 0 ,threshold = 0;
+    for( int j = 0 ; j < size ; j++)
+    {
+        if (lst[j] >= threshold)
+        {
+            if (lst[j]> max_num)
+                max_num = lst[j];
+        }
+        else 
+        {
+            lst[j] *= -1;
+            if (lst[j]> max_num)
+                max_num = lst[j];
+            
+        }
+    }
+
+    return max_num;
+
+}
