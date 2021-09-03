f20375ada2b9094021f7bea5d98fdf59e68502b0
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-04 00:29:39 +0630

    Max num in Array Lab Program

---
+++ Week 13-Arrays Lab/maxinlst.cpp
@@ -0,0 +1,11 @@
+int maxinlist(int lst[], int size)
+{   
+    int max_num = 0 ;
+    for (int j = 0 ; j < size ; j++)
+    {
+        if (lst[j]> max_num)
+            max_num = lst[j];
+    }
+
+    return max_num;
+}
