5b15bf85bf5c1bfce6043887d2fc667631ff9f85
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-04 00:32:14 +0630

    Average num in Array Lab Program

---
+++ Week 13-Arrays Lab/avgoflst.cpp
@@ -0,0 +1,13 @@
+float avgoflist(int lst[], int size)
+{
+    int sum = 0 ;
+    float average ;
+    for (int j = 0 ; j < size ; j++)
+    {
+        sum += lst[j];
+    }
+
+    average = (float)sum / (float)size;
+
+    return average;
+}
