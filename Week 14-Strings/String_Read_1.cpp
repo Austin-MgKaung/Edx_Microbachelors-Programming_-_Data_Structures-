4cb2924a63138fd602f8551e516f768244f260cc
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-04 00:52:48 +0630

    String Read only First word Program

---
+++ Week 14-Strings/String_Read_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Please enter your name: "<<endl;
+    cin>>str;
+    cout<<str<<endl;
+    // Please enter your name: 
+    // Donald Duck
+    // Donald
+    return 0;
+}
