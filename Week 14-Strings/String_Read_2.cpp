f9ea75ee4db11019c28851abdee772dec1fad3fa
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-04 00:55:03 +0630

    String Read Whole Line Program

---
+++ Week 14-Strings/String_Read_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Please enter your name: "<<endl;
+    getline(cin,str);
+    cout<<str<<endl;
+    // Please enter your name: 
+    // Donald Duck
+    // Donald Duck
+    return 0;
+}
