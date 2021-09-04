436e8a300de5630d083f4beda36fc3721768eb86
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-04 13:40:01 +0630

    Updated Runtime Analysis Example 2 Program

--- Week 11-Algorithm Analysis/runtime_analysis_2.cpp
+++ Week 11-Algorithm Analysis/runtime_analysis_2.cpp
@@ -6,6 +6,8 @@ int main()
     int n,j,i;
     cout<<"Please enter n:  "<<endl;
     cin>>n;
+    // 1/2 n^2 +1/2 n = Q(n^2)
+    //T(n) = Q(n^2)
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=i;j++)
