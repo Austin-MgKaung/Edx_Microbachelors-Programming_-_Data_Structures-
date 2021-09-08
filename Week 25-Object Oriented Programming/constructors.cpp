94b2f8fda9b527d7d1a237fdb286a67c53f054fa
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-08 20:13:47 +0630

    Constructor Example Program

---
+++ Week 25-Object Oriented Programming/constructors.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class MyClass {     // The class
+  public:           // Access specifier
+    MyClass() {     // Constructor
+      cout << "Hello World!"<<endl;
+    }
+};
+
+int main() {
+  MyClass myObj;    // Create an object of MyClass (this will call the constructor)
+  return 0;
+}
