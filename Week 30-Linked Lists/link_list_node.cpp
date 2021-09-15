e4821293ea850bb49334d62ebdd57e76d26b431f
Kaung Myat Tun <mgkaungmyattun1999@gmail.com>
2021-09-15 21:18:17 +0630

    Linked List Node Program

---
+++ Week 30-Linked Lists/link_list_node.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+template <class T>
+class LListNode 
+{
+    T data;
+    LListNode <T>* next ;
+    public:
+    LListNode(T newdata = T() ,LListNode <T> * newNext = nullptr) : data(newdata) , next(newNext){}
+    friend class LList <T>;
+};
+
